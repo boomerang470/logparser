f243db845b7e5d124f7bd589ba05475041de7734
tdttejas <tdttejas@gmail.com>
2024-01-05 14:56:34 +0530

    brain done

--- logparser/Brain/demo.py
+++ logparser/Brain/demo.py
@@ -5,15 +5,13 @@ sys.path.append('../../')
 from logparser.Brain import LogParser
 
 dataset    = 'HDFS'
-input_dir  = '../../data/loghub_2k/HDFS/' # The input directory of log file
+input_dir  = '../../data/' # The input directory of log file
 output_dir = 'demo_result/'  # The output directory of parsing results
-log_file   = 'HDFS_2k.log'  # The input log file name
-log_format = '<Date> <Time> <Pid> <Level> <Component>: <Content>'  # HDFS log format
+log_file   = 'syslog'  # The input log file name
+log_format = '<IP> <month> <date> <Timestamp> <num> <loc> <status>: <Content>'  # HDFS log format
 # Regular expression list for optional preprocessing (default: [])
 regex      = [
-    r'blk_(|-)[0-9]+' , # block id
-    r'(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)', # IP
-    r'(?<=[^A-Za-z0-9])(\-?\+?\d+)(?=[^A-Za-z0-9])|[0-9]+$', # Numbers
+	r'^(\S+)\s+(\w{3}\s+\d{2})\s+(\d{2}:\d{2}:\d{2})\s+(\S+)\s+(\S+)\s+(.*)$'
 ]
 threshold  = 2  # Similarity threshold
 delimeter  = []  # Depth of all leaf nodes
