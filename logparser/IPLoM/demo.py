43ad2df2e0ae9db0e0ee3acd0d8681c72683602f
tdttejas <tdttejas@gmail.com>
2024-01-05 15:36:24 +0530

    IPLOM done

--- logparser/IPLoM/demo.py
+++ logparser/IPLoM/demo.py
@@ -4,10 +4,10 @@ import sys
 sys.path.append('../../')
 from logparser.IPLoM import LogParser
 
-input_dir    = '../../data/loghub_2k/HDFS/'  # The input directory of log file
+input_dir    = '../../data/'  # The input directory of log file
 output_dir   = 'demo_result/'  # The output directory of parsing results
-log_file     = 'HDFS_2k.log'  # The input log file name
-log_format   = '<Date> <Time> <Pid> <Level> <Component>: <Content>'  # HDFS log format
+log_file     = 'syslog'  # The input log file name
+log_format   = '<IP> <month> <date> <Timestamp> <num> <loc> <status>: <Content>'  # HDFS log format
 maxEventLen  = 120  # The maximal token number of log messages (default: 200)
 step2Support = 0  # The minimal support for creating a new partition (default: 0)
 CT           = 0.35  # The cluster goodness threshold (default: 0.35)
