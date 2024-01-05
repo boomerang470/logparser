c86b63cd19f2346442bf71a6bad82d405c4ecc76
tdttejas <tdttejas@gmail.com>
2024-01-05 13:08:41 +0530

    AEL parsing

--- logparser/AEL/demo.py
+++ logparser/AEL/demo.py
@@ -4,13 +4,13 @@ import sys
 sys.path.append('../../')
 from logparser.AEL import LogParser
 
-input_dir     = '../../data/loghub_2k/HDFS/' # The input directory of log file
+input_dir     = '../../data/' # The input directory of log file
 output_dir    = 'demo_result/' # The output directory of parsing results
-log_file      = 'HDFS_2k.log' # The input log file name
-log_format    = '<Date> <Time> <Pid> <Level> <Component>: <Content>' # HDFS log format
+log_file      = 'syslog' # The input log file name
+log_format    = '<IP> <month> <date> <Timestamp> <num> <loc> <status>: <Content>' # HDFS log format
 minEventCount = 2 # The minimum number of events in a bin
 merge_percent = 0.5 # The percentage of different tokens
-regex         = [r'blk_-?\d+', r'(\d+\.){3}\d+(:\d+)?'] # Regular expression list for optional preprocessing (default: [])
+regex         = [r'^(\S+)\s+(\w{3}\s+\d{2})\s+(\d{2}:\d{2}:\d{2})\s+(\S+)\s+(\S+)\s+(.*)$'] # Regular expression list for optional preprocessing (default: [])
 
 parser = LogParser(input_dir, output_dir, log_format, rex=regex,
                    minEventCount=minEventCount, merge_percent=merge_percent)
