aa9bcf254a9a8991306ef3e033dd7a63cb766f5f
harsh <harshsingh7578@gmail.com>
2024-01-09 00:22:31 +0530

    NuLog results added

--- logparser/NuLog/demo.py
+++ logparser/NuLog/demo.py
@@ -4,16 +4,12 @@ import sys
 sys.path.append('../../')
 from logparser.NuLog import LogParser
 
-input_dir = '../../data/loghub_2k/HDFS/' # The input directory of log file
+input_dir = '../../data/loghub_2k/Windows/' # The input directory of log file
 output_dir = 'demo_result/'  # The output directory of parsing results
-log_file = 'HDFS_2k.log'  # The input log file name
-log_format = '<Date> <Time> <Pid> <Level> <Component>: <Content>'  # HDFS log format
+log_file = 'syslog.log'  # The input log file name
+log_format = '<IP> <month> <date> <Timestamp> <num> <loc> <status>: <Content>'  # HDFS log format
 # Regular expression list for optional preprocessing (default: [])
-regex = [
-    r'blk_(|-)[0-9]+' , # block id
-    r'(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)', # IP
-    r'(?<=[^A-Za-z0-9])(\-?\+?\d+)(?=[^A-Za-z0-9])|[0-9]+$', # Numbers
-]
+regex = [r'^(\S+)\s+(\w{3}\s+\d{2})\s+(\d{2}:\d{2}:\d{2})\s+(\S+)\s+(\S+)\s+(.*)$']
 filters = "(\s+blk_)|(:)|(\s)"
 k = 15
 nr_epochs = 5 # Number of epochs to run
